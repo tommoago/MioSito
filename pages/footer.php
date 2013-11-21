8b70ccc45ff694aaac315ab77ea1e4e1df96eb22
tommoago <tommo.ago@gmail.com>
2013-11-21 16:06:13 +0100

    Primo commit

---
+++ pages/footer.php
@@ -0,0 +1,17 @@
+<footer>
+	<ul>
+		<li>
+			<a href="Index.php">Home</a>
+		</li>
+		<li>
+			<a href="Index.php">CV</a>
+		</li>
+		<li>
+			<a href="Index.php">Cosa faccio</a>
+		</li>
+		<li>
+			<a href="Index.php">Contatti</a>
+		</li>
+	</ul>
+</footer>
+</div></body></html>
---
+++ pages/header.php
@@ -0,0 +1,31 @@
+<html>
+
+	<head>
+		<title>Tommaso Agostini</title>
+		<link href="../style/style.css" rel="stylesheet">
+	</head>
+	<body><div id="page">
+		<header>
+			<div id="hsin">
+				<img src="../images/logo.png" alt="<h1>TommasoAgostini</h1>" id="logo">
+			</div>
+			<div id="hdes">
+				<ul>
+
+					<li>
+						<a href="">Contatti</a>
+					</li>
+					<li>
+						<a href="">Cosa faccio</a>
+					</li>
+					<li>
+						<a href="">CV</a>
+					</li>
+					<li>
+						<a href="index.php">Home</a>
+					</li>
+				</ul>
+			</div>
+			<div id="hgiu"></div>
+
+		</header>
