a04fea1f5d9f9eab20b5e9fb37a5fdd54920cbde
tommoago <tommo.ago@gmail.com>
2013-11-26 21:15:58 +0100

    Nuova struttura

--- pages/Index.php
+++ pages/Index.php
@@ -1,7 +1,12 @@
 <?php
 include 'header.php';
 ?>
-<div id="content"></div>
-<?php
-include 'footer.php';
-?>
+<div id="par1"></div>
+<div id="par2"></div>
+<div id="par3"></div>
+<div id="par4"></div>
+
+</div>
+</body>
+</html>
+
--- pages/header.php
+++ pages/header.php
@@ -3,29 +3,63 @@
 	<head>
 		<title>Tommaso Agostini</title>
 		<link href="../style/style.css" rel="stylesheet">
-	</head>
-	<body><div id="page">
-		<header>
-			<div id="hsin">
-				<img src="../images/logo.png" alt="<h1>TommasoAgostini</h1>" id="logo">
-			</div>
-			<div id="hdes">
-				<ul>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				var h = $(window).height();
 
-					<li>
-						<a href="">Contatti</a>
-					</li>
-					<li>
-						<a href="">Cosa faccio</a>
-					</li>
-					<li>
-						<a href="">CV</a>
-					</li>
-					<li>
-						<a href="index.php">Home</a>
-					</li>
-				</ul>
+				$('#home').on('click', function() {
+					$('html, body').animate({
+						scrollTop : 0
+					}, 1000);
+				});
+
+				$('#cv').on('click', function() {
+					$('html, body').animate({
+						scrollTop : h
+					}, 1000);
+				});
+
+				$('#cosa').on('click', function() {
+					$('html, body').animate({
+						scrollTop : h * 2
+					}, 1000);
+				});
+
+				$('#contatti').on('click', function() {
+					$('html, body').animate({
+						scrollTop : h * 3
+					}, 1000);
+				});
+
+			});
+		</script>
+	</head>
+	<body>
+
+		<header>
+			<div id="nav">
+				<div id="hsin">
+					<div id="logo"></div>
+				</div>
+				<div id="hdes">
+					<ul>
+
+						<li id="contatti">
+							Contatti
+						</li>
+						<li id="cosa">
+							Cosa faccio
+						</li>
+						<li id="cv">
+							CV
+						</li>
+						<li id="home">
+							Home
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div id="hgiu"></div>
 
 		</header>
+		<div id="page">
